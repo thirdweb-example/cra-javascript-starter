cf5bea3c0cf99c3abb8676cddfeabfdb2be097e4
Jarrod Watts <35651410+jarrodwatts@users.noreply.github.com>
2022-08-29 12:47:41 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
+import { ConnectWallet } from "@thirdweb-dev/react";
 
-function App() {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+export default function Home() {
   return (
-    <div>
-      {address ? (
-        <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
-          <p>Your address: {address}</p>
-        </>
-      ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
-      )}
-    </div>
+      <ConnectWallet accentColor="#f213a4" colorMode="light" />
   );
 }
-
-export default App;
