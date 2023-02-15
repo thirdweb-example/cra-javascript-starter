be41f12a16e434991403571cd015446971606ab4
Jonas Daniels <jonas.daniels@outlook.com>
2023-02-15 14:16:03 -0800

    prepare for anyevm launch

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
 
-// This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+// This is the chain your dApp will work on.
+// Change this to the chain your app is built for.
+// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
+const activeChain = "ethereum";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider activeChain={activeChain}>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
