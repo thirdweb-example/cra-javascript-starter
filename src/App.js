61945192d97e69f8277df35d79a0517ef8bdbd9e
Kevin Canlas <kvncnlsdsgns@gmail.com>
2023-06-27 23:32:36 +0100

    Fixed styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <main className="main">
+    <main className="main">
+      <div className="container">
         <div class="header">
           <h1 className="title">
             Welcome to{" "}
@@ -14,20 +14,27 @@ export default function Home() {
           </h1>
 
           <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
+            Get started by configuring your desired network
+            in <code className="code">src/index.js</code>,
+            then modify the{" "}
             <code className="code">src/App.js</code> file!
           </p>
 
           <div className="connect">
             <ConnectWallet
-              dropdownPosition={{ side: "bottom", align: "center" }}
+              dropdownPosition={{
+                side: "bottom",
+                align: "center",
+              }}
             />
           </div>
         </div>
 
         <div className="grid">
-          <a href="https://portal.thirdweb.com/" className="card">
+          <a
+            href="https://portal.thirdweb.com/"
+            className="card"
+          >
             <img
               src="/images/portal-preview.png"
               alt="Placeholder preview of starter"
@@ -35,36 +42,51 @@ export default function Home() {
             <div className="card-text">
               <h2 className="gradient-text-1">Portal ➜</h2>
               <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
+                Guides, references, and resources that will
+                help you build with thirdweb.
               </p>
             </div>
           </a>
 
-          <a href="https://thirdweb.com/dashboard" className="card">
+          <a
+            href="https://thirdweb.com/dashboard"
+            className="card"
+          >
             <img
               src="/images/dashboard-preview.png"
               alt="Placeholder preview of starter"
             ></img>
-            <h2 className="gradient-text-2">Dashboard ➜</h2>
-            <p>
-              Deploy, configure, and manage your smart contracts from the
-              dashboard.
-            </p>
+            <div className="card-text">
+              <h2 className="gradient-text-2">
+                Dashboard ➜
+              </h2>
+              <p>
+                Deploy, configure, and manage your smart
+                contracts from the dashboard.
+              </p>
+            </div>
           </a>
 
-          <a href="https://thirdweb.com/templates" className="card">
+          <a
+            href="https://thirdweb.com/templates"
+            className="card"
+          >
             <img
               src="/images/templates-preview.png"
               alt="Placeholder preview of templates"
             ></img>
-            <h2 className="gradient-text-3">Templates ➜</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
+            <div className="card-text">
+              <h2 className="gradient-text-3">
+                Templates ➜
+              </h2>
+              <p>
+                Discover and clone template projects
+                showcasing thirdweb features.
+              </p>
+            </div>
           </a>
         </div>
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
