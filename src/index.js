39a57c5a0c854e74a241016abb3775d50e7ec6a7
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2023-08-28 20:48:16 +0530

    feat: updatr to use client id (#14)

--- src/index.js
+++ src/index.js
@@ -14,7 +14,10 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
+    <ThirdwebProvider
+      activeChain={activeChain}
+      clientId={process.env.REACT_APP_TEMPLATE_CLIENT_ID}
+    >
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
